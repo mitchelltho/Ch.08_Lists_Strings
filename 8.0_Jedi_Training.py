ad7fcee938a78e20afc371b3120d4421b042f3c2
mitchelltho <mitchelltho@urbandaleschools.com>
2023-12-08 09:19:29 -0600

    Sussy commit message

--- 8.0_Jedi_Training.py
+++ 8.0_Jedi_Training.py
@@ -8,20 +8,38 @@ Don't use that. Sum the numbers individually as shown in the chapter.
 Also, a common mistake is to calculate the average each time through the loop 
 to add the numbers. Finish adding the numbers before you divide.
 '''
+'''
 a_list = [3,12,3,5,3,4,6,8,5,3,5,6,3,2,4]
 b_list = [4,15,2,7,8,3,1,10,9]
 c_list = [5,10,13,12,5,9,2,6,1,8,8,9,11,13,14,8,2,2,6,3,9,8,10]
 
 
+total = 0
+chosen-list = input("Which list? ").upper()
+if chosen-list == "A":
+    chosen-list = a_list
+elif chosen-list == "B":
+    chosen-list = b_list
+elif chosen-list == "C":
+    chosen-list = c_list
+for i in chosen-list:
 
+    total += i
+
+print(total / len(chosen-list))
+'''
 '''
 2.) USERNAME:  (3pts)
 Write a program that will strip the username (whatever is in front of the @ symbol)
 from any e-mail address and print it. First ask the user for their e-mail address.
 '''
-
-
-
+'''
+email = input("What is your email? ")
+for char in email:
+    if char == "@":
+        break
+    print(char)
+'''
 '''
 TEXT FORMATTING:  (4pts)
 3.) Make following program output the following:
@@ -32,11 +50,12 @@ TEXT FORMATTING:  (4pts)
      Do not use any plus sign (+) in your code.
      You should only have two double quotes in each print statement.
      '''
+'''
 score = 41237
 highscore = 1023407
-print("Score:      " + str(score) )
-print("High score: " + str(highscore) )
-
+print("Score:      " + str(f"{score:,}"))
+print("High score: " + str(f"{highscore:,}"))
+'''
 
 '''
 4.) MONTHS PROGRAM   (5pts)
@@ -45,11 +64,15 @@ From the user input number, slice the string below in your program to print the 
 Keep repeating this until the user enters a non 1-12 number to quit.
 Once the user quits, print "Goodbye!"
 '''
-
+'''
 months = "JanFebMarAprMayJunJulAugSepOctNovDec"
 
+monthnum = int(input("Month Number? "))
+month = monthnum * 3
+monthlast = month + 3
 
-
+print(months[month - 3:monthlast - 3])
+'''
 '''
 5.) DECRYPTION PROGRAM   (5pts)
 An ENCRYPTION program was used to generate the following secret code. The encryption program converted each character 
@@ -62,3 +85,14 @@ along with the shift number?
 '''
 Secret_Message="Lxwp{j}~uj}rxw|*)bx~)l{jltnm)}qn)lxmn7)]qn)ox{ln)r|)\][XWP)r}q)x~*"
 
+
+'''
+for i in range(41):
+    encrypted_text = ""
+    for c in Secret_Message:
+        x = ord(c)
+        x = x + i -20
+        c2 = chr(x)
+        encrypted_text = encrypted_text + c2
+    print(encrypted_text)
+'''
